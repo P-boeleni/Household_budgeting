e5a148edc950f32732882f986ae00d9b2fd98723
P-boeleni <ita.ark.takahiroh@gmail.com>
2023-06-14 14:13:37 +0900

    springbootフォルダの作成

---
+++ householdBudgetingAPI/src/main/java/com/expenses/householdBudgetingAPI/HouseholdBudgetingApiApplication.java
@@ -0,0 +1,13 @@
+package com.expenses.householdBudgetingAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseholdBudgetingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseholdBudgetingApiApplication.class, args);
+	}
+
+}
